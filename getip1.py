57b17623d615daae11cda2d20a5ff9d61787e49b
cyberspidy1 <36754635+cyberspidy1@users.noreply.github.com>
2021-03-20 19:09:18 +0530

    Initial commit

---
+++ getip1.py
@@ -0,0 +1,14 @@
+import socket
+import sys
+hostname = socket.gethostname()
+ipaddr = socket.gethostbyname(hostname)
+print(hostname , ipaddr)
+file = open('myip.txt', 'w')
+file.write(ipaddr)
+file.close()
+ipsplit = ipaddr.split(".")
+a = '{0}.{1}.{2}.0/24'.format(ipsplit[0], ipsplit[1], ipsplit[2])
+print(a)
+file = open('hostname_ip.txt', 'w')
+file.write(hostname+ ' ' + ipaddr)
+file.close()
