d9c95ab241f255bcc1931e39d4833f937c8309b0
cyberspidy1 <36754635+cyberspidy1@users.noreply.github.com>
2021-03-21 13:34:44 +0530

    Host identification using ping scan

--- getip1.py
+++ getip1.py
@@ -1,5 +1,6 @@
 # th3Scann3r
 import socket
+import networkscan
 from datetime import datetime
 
 hostname = socket.gethostname()
@@ -9,33 +10,17 @@ file = open('myip.txt', 'w')
 file.write(ipaddr)
 file.close()
 ipsplit = ipaddr.split(".")
-# a = '{0}.{1}.{2}.0/24'.format(ipsplit[0], ipsplit[1], ipsplit[2])
-# print(a)
-ipsplit2 = ipsplit[0] + '.' + ipsplit[1] + '.' + ipsplit[3] + '.'
-stn1 = 1
-edn1 = 2
+a = '{0}.{1}.{2}.0/24'.format(ipsplit[0], ipsplit[1], ipsplit[2])
+print(a)
 td1 = datetime.now()
-print("scan has been initiated on", td1)
+print("scan is initiated on", td1)
+if __name__ == '__main__':
+    network = a
+    my_scan = networkscan.Networkscan(network)
+    my_scan.run()
+    for i in my_scan.list_of_hosts_found:
+        print(i)
 
-
-def scan(addr):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    result = sock.connect((addr, 80))
-    if result == 0:
-       return 0
-    else:
-       return 1
-
-
-def run1():
-    for ip in range(stn1, edn1):
-        addr = ipsplit2 + str(ip)
-        if scan((addr)):
-            print(addr, "this address is live")
-
-
-run1()
 td2 = datetime.now()
 total = td2 - td1
 print("scanning completed in", total)
