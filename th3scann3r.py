b66585d28b1f1b650a4dcc554cc3d396dea63c13
cyberspidy1 <36754635+cyberspidy1@users.noreply.github.com>
2021-03-22 13:43:14 +0530

    Works on Linux with python 3.7

--- th3scann3r.py
+++ th3scann3r.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3.6
 # th3Scann3r
 import socket
 import networkscan
@@ -15,9 +14,7 @@ s.connect((gw[2], 0))
 ipaddr = s.getsockname()[0]
 gateway = gw[2]
 host = socket.gethostname()
-print ("IP:", ipaddr, " GW:", gateway, " Host:", host)
-
-
+print("IP:", ipaddr, " GW:", gateway, " Host:", host)
 
 file = open('myip.txt', 'w')
 file.write(ipaddr)
