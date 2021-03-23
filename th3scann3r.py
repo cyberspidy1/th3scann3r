081ec5042641833f9e87881ebd708801238bfcaf
cyberspidy1 <36754635+cyberspidy1@users.noreply.github.com>
2021-03-23 16:14:32 +0530

    tiny enhancement

--- th3scann3r.py
+++ th3scann3r.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 hostname = socket.gethostname()
 ipaddr = socket.gethostbyname(hostname)
-print(hostname, ipaddr)
+#print(hostname, ipaddr)
 
 #For Linux environment#
 
@@ -25,20 +25,20 @@ file.write(ipaddr)
 file.close()
 ipsplit = ipaddr.split(".")
 a = '{0}.{1}.{2}.0/24'.format(ipsplit[0], ipsplit[1], ipsplit[2])
-print(a)
+#print(a)
+print("-------th3scann3r--------")
 td1 = datetime.now()
 print("scan is initiated on", td1)
 
 if __name__ == '__main__':
     my_scan = networkscan.Networkscan(a)
+    print("netowrk to scan: " + str(my_scan.network))
+    print("prefix to scan: " + str(my_scan.network.prefixlen))
+    print("number of host to scan: " + str(my_scan.nbr_host))
     my_scan.run()
-    for i in my_scan.list_of_hosts_found:
-        print(i)
+    for b in my_scan.list_of_hosts_found:
+        print(b)
 
 td2 = datetime.now()
 total = td2 - td1
 print("scanning completed in", total)
-
-#file = open('hostname_ip.txt', 'w')
-#file.write(hostname + ' ' + ipaddr)
-#file.close()
